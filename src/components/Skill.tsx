38be3845436de0350eee1a4673938918892871cb
Taisuke Sakugawa <taisuke140909@gmail.com>
2026-01-22 14:34:14 +0900

    improve contents

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -4,7 +4,7 @@ import SkillStars, { type StarLevel } from "./SkillStars";
 export default function Skill(props: { name: string; level: StarLevel; right?: JSX.Element }) {
     return (
         <li class="flex items-center justify-between gap-4">
-            <span class="min-w-0 truncate font-medium text-slate-800">{props.name}</span>
+            <span class="min-w-0 break-words font-medium text-slate-800">{props.name}</span>
             {props.right ?? <SkillStars level={props.level} />}
         </li>
     );
